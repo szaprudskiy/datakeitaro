936cdc8f798625da254d2adf8ef4a6840e9d1383
szaprudskiy <sergei.zaprudskiy@gmail.com>
2023-08-02 15:13:21 +0300

    urll

--- frontend/js/getData.js
+++ frontend/js/getData.js
@@ -3,7 +3,7 @@ const getData = async () => {
   const landingId = document.getElementById('landingId').value
   const dateFrom = document.getElementById('dateFrom').value
   const dateTo = document.getElementById('dateTo').value
-  //url
+  //urll
   const apiUrl = 'https://datakeitaro.vercel.app/getdata'
 
   try {
