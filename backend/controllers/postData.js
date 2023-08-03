7d1723922924e0a5ddf2f34e45d7f155a7149f07
szaprudskiy <sergei.zaprudskiy@gmail.com>
2023-08-03 11:14:23 +0300

    back check land campaign

--- backend/controllers/postData.js
+++ backend/controllers/postData.js
@@ -5,81 +5,77 @@ const postData = async (req, res) => {
     const { campaignId, landingId, dateFrom, dateTo } = req.body
     const apiUrl = process.env.URL + `/admin_api/v1/clicks/log`
 
-    const response = await axios.post(
-      apiUrl,
-      {
-        range: {
-          from: dateFrom,
-          to: dateTo,
-          timezone: 'Europe/Moscow',
-          interval: null,
-        },
-        limit: 0,
-        offset: 0,
-        columns: [
-          'sub_id',
-          'datetime',
-          'ip',
-          'campaign_id',
-          'stream_id',
-          'landing',
-          'offer',
-          'ts',
-          'affiliate_network',
-          'country_flag',
-          'region',
-          'city',
-          'os_icon',
-          'browser_icon',
-          'connection_type',
-          'device_type',
-          'device_model',
-          'is_bot',
-          'is_unique_campaign',
-          'sub_id_16',
-          'sub_id_18',
-          'sub_id_17',
-          'sub_id_19',
-          'sub_id_20',
-          'sub_id_21',
-          'sub_id_22',
-          'sub_id_23',
-          'sub_id_24',
-          'sub_id_25',
-          'sub_id_26',
-          'sub_id_27',
-          'sub_id_28',
-          'sub_id_29',
-          'sub_id_30',
-          'is_lead',
-          'is_sale',
-          'is_rejected',
-        ],
-        filters: [
-          {
-            name: 'campaign_id',
-            operator: 'EQUALS',
-            expression: campaignId,
-          },
-          {
-            name: 'landing_id',
-            operator: 'EQUALS',
-            expression: landingId,
-          },
-        ],
-        // "sort": [
-        //   {
-        //     "name": "string",
-        //     "order": "ASC"
-        //   }
-        // ]
+    const requestData = {
+      range: {
+        from: dateFrom,
+        to: dateTo,
+        timezone: 'Europe/Moscow',
+        interval: null,
       },
-      {
-        headers: {
-          'Api-Key': process.env.API_KEY,
-        },
-      }
-    )
+      limit: 0,
+      offset: 0,
+      columns: [
+        'sub_id',
+        'datetime',
+        'ip',
+        'campaign_id',
+        'stream_id',
+        'landing',
+        'offer',
+        'ts',
+        'affiliate_network',
+        'country_flag',
+        'region',
+        'city',
+        'os_icon',
+        'browser_icon',
+        'connection_type',
+        'device_type',
+        'device_model',
+        'is_bot',
+        'is_unique_campaign',
+        'sub_id_16',
+        'sub_id_18',
+        'sub_id_17',
+        'sub_id_19',
+        'sub_id_20',
+        'sub_id_21',
+        'sub_id_22',
+        'sub_id_23',
+        'sub_id_24',
+        'sub_id_25',
+        'sub_id_26',
+        'sub_id_27',
+        'sub_id_28',
+        'sub_id_29',
+        'sub_id_30',
+        'is_lead',
+        'is_sale',
+        'is_rejected',
+      ],
+      filters: [],
+    }
+    if (campaign_id) {
+      requestData.filters.push({
+        name: 'campaign_id',
+        operator: 'EQUALS',
+        expression: campaignId,
+      })
+    }
+
+    if (landingId) {
+      requestData.filters.push({
+        name: 'landing_id',
+        operator: 'EQUALS',
+        expression: landingId,
+      })
+    }
+
+    const response = await axios.post(apiUrl, requestData, {
+      headers: {
+        'Api-Key': process.env.API_KEY,
+      },
+    })
 
     console.log(response.data)
     res.json({ data: response.data })
